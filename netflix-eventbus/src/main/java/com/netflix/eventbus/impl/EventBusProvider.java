080f888eda571fbc4fd6afa52114e7e78b52ef02
elandau <elandau@yahoo.com>
2014-01-22 11:22:40 -0800

    Undeprecated EventBusProvider

--- netflix-eventbus/src/main/java/com/netflix/eventbus/impl/EventBusProvider.java
+++ netflix-eventbus/src/main/java/com/netflix/eventbus/impl/EventBusProvider.java
@@ -8,10 +8,7 @@ import com.netflix.eventbus.spi.EventBus;
  *
  * @author Nitesh Kant (nkant@netflix.com)
  * 
- * @deprecated Either create an instance of EventBusImpl directly or use a DI library to 
- * inject it as a singleton
  */
-@Deprecated
 public class EventBusProvider {
 
     private static final EventBus bus = new EventBusImpl();
